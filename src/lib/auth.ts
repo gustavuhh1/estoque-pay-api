59eab8e3a344daf06b4e62bee22ba9c708b978cb
GustavoMRodrigues <gustavomrodrigues11@gmail.com>
2026-09-10 22:38:10 -0300

    feat: implementar lógica de redefinição de senha e integração com serviço de envio de e-mails para mecanismo esqueceu-senha (#37, #38)

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,8 @@
-import { betterAuth } from "better-auth"
-import "dotenv/config"
-import { prismaAdapter } from "better-auth/adapters/prisma"
-import { prisma } from "./prisma"
+import { betterAuth } from "better-auth";
+import "dotenv/config";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+import { resend } from "./resend";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -11,7 +12,26 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: true,
     minPasswordLength: 8,
+    sendResetPassword: async ({ user, url, token }, request) => {
+      await resend.emails.send({
+        // TODO: adicionar email de remetente válido
+        from: "[EMAIL_ADDRESS]",
+        to: user.email,
+        subject: "Reset your password",
+        html: `<h1>Reset your password</h1>
+        <p>Click the link to reset your password: ${url}</p>
+        <p>Seu token é: ${token}</p>
+        <p>Se você não solicitou essa redefinição de senha, ignore este email.</p>
+        <p>Este link expirará em 1 hora.</p>
+        <p>Obrigado!</p>
+        <p>Equipe Estoque Pay</p>`,
+      });
+    },
+    onPasswordReset: async ({ user }, request) => {
+      console.log(`Password for user ${user.email} has been reset.`);
+    },
   },
+  // TODO: Implementar a autenticação social com Google.
   socialProviders: {
     // google: {
     //   clientId: process.env.GOOGLE_CLIENT_ID || "",
@@ -19,4 +39,4 @@ export const auth = betterAuth({
     //   prompt: "select_account",
     // },
   },
-})
+});
